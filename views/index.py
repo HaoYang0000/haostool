94d1c3ceae1ca3a6b0f8f9e8313f73948d2f6645
Hao <HaoYang0000@users.noreply.github.com>
2019-01-06 23:25:07 -0600

    Add fun url endpoint

--- views/index.py
+++ views/index.py
@@ -17,6 +17,14 @@ def health():
 def index():
 	return render_template('index.html')
 
+@app.route('/<whatever>/<int:times>', methods=['GET', 'POST'])
+def lol(whatever, times):
+	return_list = []
+	for x in range(times):
+		return_list.append(whatever)
+
+	return render_template('whatever.html', whatever=return_list)
+
 @app.errorhandler(Exception)
 def exception_handler(error):
     return "!!!!"  + repr(error)
