eb68328d7f2c30a171de29fa5da8ceae891ffa21
Hao <HaoYang0000@users.noreply.github.com>
2019-01-09 00:52:53 -0600

    First workable version on GAE

--- main.py
+++ main.py
@@ -46,6 +46,8 @@ def create_app():
 
     return app
 
+app = create_app()
+
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
