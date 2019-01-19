5176eb80fef71e16f0b2d9c8b72ac4e69385914c
Hao <HaoYang0000@users.noreply.github.com>
2019-01-19 00:04:40 -0600

    Get socket to work, first version

--- app/views/index.py
+++ app/views/index.py
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 
 controller = Controller(current_user=current_user)
 
+
 @app.route('/health', methods=['GET', 'POST'])
 def health():
     return 'OK', status.HTTP_201_CREATED
@@ -20,25 +21,29 @@ def health():
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-	if not current_user.is_authenticated:
-		return redirect(url_for('auth.login'))
-	else:
-		all_service = controller.get_all_service()
-		return render_template('index.html', current_user=current_user, all_service=all_service)
-	# return render_template('index.html', title='Home Page', posts=posts)
+    if not current_user.is_authenticated:
+        return redirect(url_for('auth.login'))
+    else:
+        all_service = controller.get_all_service()
+        return render_template('index.html', current_user=current_user, all_service=all_service)
+
+
+# return render_template('index.html', title='Home Page', posts=posts)
 
 @app.route('/kglb/<whatever>/<int:times>', methods=['GET', 'POST'])
 def lol(whatever, times):
-	return_list = []
-	for x in range(times):
-		return_list.append(whatever)
-	return render_template('whatever.html', whatever=return_list)
+    return_list = []
+    for x in range(times):
+        return_list.append(whatever)
+    return render_template('whatever.html', whatever=return_list)
+
 
 @app.errorhandler(401)
 def login_required(e):
     # note that we set the 404 status explicitly
-	return redirect(url_for('auth.login')), 401
+    return redirect(url_for('auth.login')), 401
+
 
 @app.errorhandler(Exception)
 def exception_handler(error):
-    return "!!!!"  + repr(error)
+    return "!!!!" + repr(error)
