52b7dd9beb0c0c5655d7fea776189278367d9182
Hao <HaoYang0000@users.noreply.github.com>
2019-01-22 23:15:06 -0600

    Adding auto selection for tag, first workable version

--- app/static/js/accounting/main.js
+++ app/static/js/accounting/main.js
@@ -12,6 +12,71 @@ function calculate_cost_by_tag(e) {
 	xhttp.send();
 }
 
+function sort_current_tag_list_by_similarity(e) {
+    var data = tags['result'];
+
+    for (var i = 0; i < data.length; i++) {
+        data[i]['similarity'] = similarity(e.value, data[i]['name']);
+    }
+    data.sort(function(a, b) {
+        return b['similarity']- a['similarity'];
+    });
+
+    var tags_drop_down_list = document.getElementById("account_tag_drop_down");
+
+    tags_drop_down_list.options.length = 0;
+
+    for (var i=0; i<data.length; i++){
+        var option = document.createElement("option");
+        option.text = data[i]['name'];
+        option.value = data[i]['id'];
+        tags_drop_down_list.appendChild(option);
+    }
+    tags_drop_down_list.options[0].selected = true;
+}
+
+function similarity(s1, s2) {
+  var longer = s1;
+  var shorter = s2;
+  if (s1.length < s2.length) {
+    longer = s2;
+    shorter = s1;
+  }
+  var longerLength = longer.length;
+  if (longerLength == 0) {
+    return 1.0;
+  }
+  return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+}
+
+function editDistance(s1, s2) {
+  s1 = s1.toLowerCase();
+  s2 = s2.toLowerCase();
+
+  var costs = new Array();
+  for (var i = 0; i <= s1.length; i++) {
+    var lastValue = i;
+    for (var j = 0; j <= s2.length; j++) {
+      if (i == 0)
+        costs[j] = j;
+      else {
+        if (j > 0) {
+          var newValue = costs[j - 1];
+          if (s1.charAt(i - 1) != s2.charAt(j - 1))
+            newValue = Math.min(Math.min(newValue, lastValue),
+              costs[j]) + 1;
+          costs[j - 1] = lastValue;
+          lastValue = newValue;
+        }
+      }
+    }
+    if (i > 0)
+      costs[s2.length] = lastValue;
+  }
+  return costs[s2.length];
+}
+
+
 
 function addData(chart, label, data) {
     chart.data.labels.push(label);
