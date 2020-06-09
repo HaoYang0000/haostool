758ad5c85a5b0ac4915d5a5d3f1c040488e8c1b0
Hao <yanghao0613@gmail.com>
2020-06-09 20:15:19 +0800

    remove old streaming, make change to current streaming

--- app/views/streaming/streaming.py
+++ app/views/streaming/streaming.py
@@ -6,27 +6,14 @@ from flask import Blueprint, jsonify, render_template, session, url_for, redirec
 from flask_api import status
 from flask_login import current_user, login_user, logout_user
 from flask_cors import cross_origin
-# from app.services.streaming.streaming import Camera
+from app.engine import UPLOAD_ROOT
 
 app = Blueprint('streaming', __name__)
 logger = logging.getLogger(__name__)
 
-# def gen(camera):
-#     """
-#     流媒体发生器
-#     """
-#     while True:
-#         frame = camera.get_frame()
-
-#         yield (b'--frame\r\n'
-#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-# @app.route('/video_feed', methods=['GET', 'POST'])
-# def video_feed():
-#     return Response(gen(Camera()),
-#                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/streaming', methods=['GET', 'POST'])
 @cross_origin()
 def streaming():
-    return render_template('streaming/streaming.html')
+    is_streaming = os.path.exists(f"{UPLOAD_ROOT}/live/index.m3u8")
+    return render_template('streaming/streaming.html', is_streaming=is_streaming)
