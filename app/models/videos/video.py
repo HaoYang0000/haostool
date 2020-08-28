a9823d46844a51212d5ec70ec2b78b87b6714a89
Hao <yanghao0613@gmail.com>
2020-08-28 17:40:52 +0800

    add fallguys as new video type

--- app/models/videos/video.py
+++ app/models/videos/video.py
@@ -40,7 +40,7 @@ class VideoModel(BaseModelExtended):
         default=1
     )
     category = Column(
-        Enum('dota', 'pubg', 'blog'),
+        Enum('dota', 'pubg', 'fallguys', 'blog'),
         nullable=False
     )
 
