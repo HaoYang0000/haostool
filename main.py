ebf6fbab89544bde13d97c5d66057ea5db168f03
Hao Yang <yanghao0613@gmail.com>
2021-09-01 12:50:33 +0800

    update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from flask_apispec import FlaskApiSpec
 from flask_sqlalchemy import SQLAlchemy
 from backend.engine import create_app, socketIO
 from flask_socketio import SocketIO
+from backend.logs.logger import logger
 
 app = create_app()
 
@@ -17,5 +18,5 @@ if __name__ == '__main__':
     # the "static" directory. See:
     # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
     # App Engine itself will serve those files as configured in app.yaml.
-    # app.run(host='127.0.0.1', port=8080, debug=True)
-    socketIO.run(app, host='0.0.0.0', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
+    # socketIO.run(app, host='0.0.0.0', port=8080, debug=True)
